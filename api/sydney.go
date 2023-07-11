aa548eb6b5519484c1d868acc8f5f69e9183558d
Hisune <hi@hisune.com>
2023-07-11 17:07:27 +0800

    Update sydney.go

--- api/sydney.go
+++ api/sydney.go
@@ -4,6 +4,7 @@ import (
 	"adams549659584/go-proxy-bingai/api/helper"
 	"adams549659584/go-proxy-bingai/common"
 	"net/http"
+	"fmt"
 )
 
 func Sydney(w http.ResponseWriter, r *http.Request) {
@@ -11,5 +12,7 @@ func Sydney(w http.ResponseWriter, r *http.Request) {
 		helper.UnauthorizedResult(w)
 		return
 	}
+	q := req.URL.Query()
+	fmt.Println(q)
 	common.NewSingleHostReverseProxy(common.BING_SYDNEY_URL).ServeHTTP(w, r)
 }
