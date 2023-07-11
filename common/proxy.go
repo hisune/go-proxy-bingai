17150ba7e91f40ed8478a8fce15aa09a4f124b55
Hisune <hi@hisune.com>
2023-07-11 16:55:52 +0800

    Update proxy.go

--- common/proxy.go
+++ common/proxy.go
@@ -268,7 +268,6 @@ func getRandCookie(req *http.Request) (int, string) {
 }
 
 func replaceResBody(originalBody string, originalScheme string, originalHost string) string {
-	fmt.Println(originalBody)
 	modifiedBodyStr := originalBody
 
 	if originalScheme == "https" {
